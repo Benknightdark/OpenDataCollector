dcb583328b8ba88872aafa89db8d175d0a9cfb01
ben <moon791017@gmail.com>
2021-03-29 16:14:30 +0800

    aa

--- website/pages/components/dahsboard.tsx
+++ website/pages/components/dahsboard.tsx
@@ -2,14 +2,38 @@
 import useSWR from "swr";
 import * as Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-const options: Highcharts.Options = {
-    title: {
-        text: 'My chart'
-    },
-    series: [{
-        type: 'line',
-        data: [1, 2, 3]
-    }]
+
+const options = (rawData) => {
+    const seriesData = rawData.map(a => {
+        return { name: a.name, y: a.count }
+    });
+    return {
+        title: {
+            text: ''
+        },
+        chart: {
+            type: 'pie'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+
+        series: [{
+            name: '資料數量',
+            colorByPoint: true,
+            data: seriesData
+        }],
+        credits: {
+            enabled: false
+        }
+    }
 }
 const dashboardData = (serviceName: string) => {
     const { data, error, isValidating, mutate } = useSWR(
@@ -29,26 +53,29 @@ export default function Dashboard(props) {
             <div className="card-header">
                 {fetchDashboardData.data?.title}  </div>
             <div className="card-body">
-                <div className="d-flex p-2 bd-highlight flex-wrap">
+                <div className="d-flex p-2 bd-highlight flex-wrap justify-content-center">
                     {
                         fetchDashboardData.data && fetchDashboardData.data.items.map(d => {
-                            return <div className="p-3 bd-highlight" key={d.name}>
-                                <h3>{d.name}</h3>
-                                <h4>{d.count}筆</h4>
-                            </div>
+                            return (<div className="p-3 bd-highlight" key={d.name} style={{ borderRight: '1px solid grey' }}>
+                                <h3>
+                                    <div className="badge rounded-pill bg-primary">{d.name}</div> </h3>
+                                <h4>
+                                    <div className="animate__animated animate__flipInX text-center">{d.count}</div>
+                                </h4>
+                                <div className='text-center'>
+                                    <a href="#" className="btn btn-warning">
+                                    看更多<span className="material-icons" style={{ fontSize:'18px' }}>open_in_new</span></a>
+                                </div>
+                            </div>)
                         })
-
-
                     }
-
-
                 </div>
-                <div className="d-flex flex-row">
+                <div className="d-flex flex-wrap justify-content-center">
                     {
                         fetchDashboardData.data &&
                         <HighchartsReact
                             highcharts={Highcharts}
-                            options={options}
+                            options={options(fetchDashboardData.data.items)}
                             {...props}
                         />
                     }
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -7,19 +7,23 @@ export default function Home() {
       <Head>
         <title>OpenData Collector</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous"></link>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossOrigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
           rel="stylesheet"></link>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
       </Head>
-      <div className="container px-1">
-        <div className='row justify-content-center  px-2   no-gutters p-2'>
-          <div className='col-md-6  col-sm-12'>
-               <Dashboard serviceName='kao_service'/> 
-          </div>
-        
+      <div className="container-fluid px-1">
+        <div className='d-flex p-2 bd-highlight flex-wrap justify-content-evenly'>
+            <Dashboard serviceName='kao_service' />
+            <Dashboard serviceName='kao_service' />
         </div>
       </div>
-    </div>  
+    </div>
   )
 }
