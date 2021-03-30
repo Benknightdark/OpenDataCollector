58577345eab0d9795ba043ec50ef2aaaaabf0ae3
ben <moon791017@gmail.com>
2021-03-30 17:18:38 +0800

    aa

--- website/pages/[serviceName]/category/[type].tsx
+++ website/pages/[serviceName]/category/[type].tsx
@@ -1,7 +1,7 @@
 import CustomHeader from '../../components/custom-header'
 import { useRouter } from "next/router"
-import useSWR, { useSWRInfinite } from 'swr'
-import { useState } from 'react'
+import { useSWRInfinite } from 'swr'
+
 import { useEffect } from 'react'
 
 
@@ -14,19 +14,19 @@ export default function Type() {
     const getKey = (pageIndex, previousPageData) => {
         console.log(pageIndex)
         if (previousPageData && !previousPageData.length) return null
-        return `/api/dashboard?serviceName=${serviceName}&dataType=${type}&page=${pageIndex+1}`
+        return `/api/dashboard?serviceName=${serviceName}&dataType=${type}&page=${pageIndex + 1}`
 
     }
     const { data, size, setSize } = useSWRInfinite(getKey, fetcher,)
     console.log(data)
-        useEffect(() => {
+    useEffect(() => {
         window.onscroll = async () => {
-          console.log((window.innerHeight + window.scrollY) - document.body.offsetHeight )
-          if ((window.innerHeight + window.scrollY) - document.body.offsetHeight >0) {
-            setSize(size + 1)
-          }
+            console.log((window.innerHeight + window.scrollY) - document.body.offsetHeight)
+            if ((window.innerHeight + window.scrollY) - document.body.offsetHeight > 0) {
+                setSize(size + 1)
+            }
         };
-      })
+    })
     if (!data) return 'loading';
 
 
@@ -35,42 +35,44 @@ export default function Type() {
 
         <div>
             <CustomHeader goBack='true' />
-            <div className="d-flex mb-3 p-2 bd-highlight flex-wrap justify-content-startalign-content-stretch">
-                {
+            <div className="container-fluid px-1">
+                <div className="d-flex mb-3 p-2 bd-highlight flex-wrap justify-content-md-start justify-content-lg-start justify-content-sm-center justify-content-xs-center">
+                    {
 
-                    data.map((lists, index) => {
-                        return lists.map(d => <div className='p-2' key={d.name}>
-                            <div className="card mb-3 p-2" style={{ width: '300px' }}>
-                                <img src={d.image} className="card-img-top" style={{ maxWidth: '100%', height: 'auto' }} />
-                                <hr></hr>
-                                <div className="card-body">
-                                    <h5 className="card-title">{d.title}</h5>
-                                    {
-                                        d.count > 0 && <div className="skillbar clearfix ">
-                                            <div className="skillbar-title">
-                                                <span>資料數量</span>
+                        data.map((lists, index) => {
+                            return lists.map(d => <div className='p-2' key={d.name}>
+                                <div className="card mb-3 p-2" style={{ width: '300px' }}>
+                                    <img src={d.image} className="card-img-top" style={{ maxWidth: '100%', height: 'auto' }} />
+                                    <hr></hr>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{d.title}</h5>
+                                        {
+                                            d.count > 0 && <div className="skillbar clearfix ">
+                                                <div className="skillbar-title">
+                                                    <span>資料數量</span>
+                                                </div>
+                                                <div className="skillbar-bar"></div>
+                                                <div className="skill-bar-percent">{d.count}</div>
                                             </div>
-                                            <div className="skillbar-bar"></div>
-                                            <div className="skill-bar-percent">{d.count}</div>
-                                        </div>
-                                    }
-                                    <button className="btn btn-warning"
-                                        onClick={() => {
-                                            router.push(
-                                                {
-                                                    pathname: `/${serviceName}/dataset`,
-                                                    query: { queryUrl: d.url },
-                                                }
-                                            )
-                                        }}
-                                    >
-                                        看更多<span className="material-icons" style={{ fontSize: '18px' }}>open_in_new</span>
-                                    </button>
+                                        }
+                                        <button className="btn btn-warning"
+                                            onClick={() => {
+                                                router.push(
+                                                    {
+                                                        pathname: `/${serviceName}/dataset`,
+                                                        query: { queryUrl: d.url },
+                                                    }
+                                                )
+                                            }}
+                                        >
+                                            看更多<span className="material-icons" style={{ fontSize: '18px' }}>open_in_new</span>
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>)
-                    })
-                }
+                            </div>)
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
