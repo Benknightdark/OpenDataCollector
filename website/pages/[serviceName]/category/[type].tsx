753f8b5f748fb5d3c04bc34f5326efb5e0361e35
ben <moon791017@gmail.com>
2021-03-30 16:52:19 +0800

    aaa

--- website/pages/[serviceName]/category/[type].tsx
+++ website/pages/[serviceName]/category/[type].tsx
@@ -1,67 +1,94 @@
 import CustomHeader from '../../components/custom-header'
 import { useRouter } from "next/router"
-import useSWR from 'swr'
+import useSWR, { useSWRInfinite } from 'swr'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
-const dashboardData = (serviceName: string | string[], type: string | string[], page: number) => {
-    const { data, error, isValidating, mutate } = useSWR(
-        `/api/dashboard?serviceName=${serviceName}&dataType=${type}&page=${page}`,
-        fetcher,
-        {
-            refreshInterval: 60000
-        })
-    return { data, error, isValidating, mutate }
-}
-const fetcher = url => fetch(url).then(r => r.json())
+
+
 
 export default function Type() {
     const router = useRouter()
+    const fetcher = url => fetch(url).then(r => r.json())
     const { serviceName, type } = router.query
-    const fetchDashboardData = dashboardData(serviceName, type, 1);
+    // const [pageIndex, setPageIndex] = useState(1);
+    // const dashboardData = (serviceName: string | string[], type: string | string[]) => {
+    //     const { data, error, isValidating, mutate } = useSWR(
+    //         `/api/dashboard?serviceName=${serviceName}&dataType=${type}&page=${pageIndex}`,
+    //         fetcher,
+    //         {
+    //             refreshInterval: 60000
+    //         })
+    //     return { data, error, isValidating, mutate }
+    // }
+    // const fetchDashboardData = dashboardData(serviceName, type);
+    const getKey = (pageIndex, previousPageData) => {
+        if (previousPageData && !previousPageData.length) return null
+        if (pageIndex === 0) return `/api/dashboard?serviceName=${serviceName}&dataType=${type}&page=1`
+
+        // reached the end
+        return `/api/dashboard?serviceName=${serviceName}&dataType=${type}&page=${pageIndex}`
+
+        //`/users?page=${pageIndex}&limit=10`                    // SWR key
+    }
+    const { data, size, setSize } = useSWRInfinite(getKey, fetcher,)
+    console.log(data)
+    if (!data) return 'loading';
+
+
+    // useEffect(() => {
+    //     window.onscroll = async () => {
+    //       console.log((window.innerHeight + window.scrollY) - document.body.offsetHeight )
+    //       if ((window.innerHeight + window.scrollY) - document.body.offsetHeight >0) {
+    //         setSize(size + 1)
+    //       }
+    //     };
+    //   })
     return (
 
         <div>
             <CustomHeader goBack='true' />
-            {
-                fetchDashboardData.data && <div className="d-flex mb-3 p-2 bd-highlight flex-wrap justify-content-startalign-content-stretch">
-                    {
+            <div className="d-flex mb-3 p-2 bd-highlight flex-wrap justify-content-startalign-content-stretch">
+                {
 
-                        fetchDashboardData.data.map(d => {
-                            return (
-                                <div className='p-2' key={d.name}>
-                                    <div className="card mb-3 p-2" style={{ width: '300px' }}>
-                                        <img src={d.image} className="card-img-top" style={{ maxWidth: '100%', height: 'auto' }} />
-                                        <hr></hr>
-                                        <div className="card-body">
-                                            <h5 className="card-title">{d.title}</h5>
-                                            {
-                                                d.count > 0 && <div className="skillbar clearfix ">
-                                                    <div className="skillbar-title">
-                                                        <span>資料數量</span>
-                                                    </div>
-                                                    <div className="skillbar-bar"></div>
-                                                    <div className="skill-bar-percent">{d.count}</div>
-                                                </div>
-                                            }
-                                            <button className="btn btn-warning"
-                                                onClick={() => {
-                                                    router.push(
-                                                        {
-                                                            pathname: `/${serviceName}/dataset`,
-                                                            query: { queryUrl: d.url },
-                                                        }
-                                                    )
-                                                }}
-                                            >
-                                                看更多<span className="material-icons" style={{ fontSize: '18px' }}>open_in_new</span>
-                                            </button>
+                    data.map((lists, index) => {
+                        return lists.map(d => <div className='p-2' key={d.name}>
+                            <div className="card mb-3 p-2" style={{ width: '300px' }}>
+                                <img src={d.image} className="card-img-top" style={{ maxWidth: '100%', height: 'auto' }} />
+                                <hr></hr>
+                                <div className="card-body">
+                                    <h5 className="card-title">{d.title}</h5>
+                                    {
+                                        d.count > 0 && <div className="skillbar clearfix ">
+                                            <div className="skillbar-title">
+                                                <span>資料數量</span>
+                                            </div>
+                                            <div className="skillbar-bar"></div>
+                                            <div className="skill-bar-percent">{d.count}</div>
                                         </div>
-                                    </div>
+                                    }
+                                    <button className="btn btn-warning"
+                                        onClick={() => {
+                                            router.push(
+                                                {
+                                                    pathname: `/${serviceName}/dataset`,
+                                                    query: { queryUrl: d.url },
+                                                }
+                                            )
+                                        }}
+                                    >
+                                        看更多<span className="material-icons" style={{ fontSize: '18px' }}>open_in_new</span>
+                                    </button>
                                 </div>
-                            )
-                        })
-                    }
-                </div>
-            }
+                            </div>
+                        </div>)
+                    })
+
+
+
+                }
+                <button onClick={() => setSize(size + 1)}>Load More</button>
+            </div>
         </div>
     )
 }
