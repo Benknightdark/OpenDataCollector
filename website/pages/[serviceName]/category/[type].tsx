f2c7cc466141a45bf8bb370d2d1ccd01551f58e9
BenKung <moon791017@gmail.com>
2021-04-01 09:22:05 +0800

    OK

--- website/pages/[serviceName]/category/[type].tsx
+++ website/pages/[serviceName]/category/[type].tsx
@@ -1,81 +1,98 @@
-import CustomHeader from '../../components/custom-header'
-import { useRouter } from "next/router"
-import { useSWRInfinite } from 'swr'
-import Spinner from '../../components/spinner'
-import React, { useEffect, useState } from 'react'
-import Layout from '../../components/layout'
+import CustomHeader from "../../components/custom-header";
+import { useRouter } from "next/router";
+import { useSWRInfinite } from "swr";
+import Spinner from "../../components/spinner";
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/layout";
 
 export default function Type() {
-    const router = useRouter()
-    const fetcher = url => fetch(url).then(r => r.json())
-    const [showLoading, setShowLoading] = useState(false);
+  const router = useRouter();
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const [showLoading, setShowLoading] = useState(false);
 
-    const { serviceName, type } = router.query
-    const getKey = (pageIndex, previousPageData) => {
-        if (previousPageData && !previousPageData.length) return null
-        const url =`/api/category?serviceName=${serviceName}&dataType=${type}&page=${pageIndex + 1}`;
-        console.log(url)
-        return url
-    }
-    const { data, size, setSize } = useSWRInfinite(getKey, fetcher,)
-    useEffect(() => {
-        window.onscroll = async () => {
-            if (showLoading) return;
+  const { serviceName, type } = router.query;
+  const getKey = (pageIndex, previousPageData) => {
+    if (previousPageData && !previousPageData.length) return null;
+    const url = `/api/category?serviceName=${serviceName}&dataType=${type}&page=${
+      pageIndex + 1
+    }`;
+    console.log(url);
+    return url;
+  };
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher);
+  useEffect(() => {
+    window.onscroll = async () => {
+      if (showLoading) return;
 
-            if ((window.innerHeight + window.scrollY) - document.body.offsetHeight > 0) {
-                setShowLoading(true)
-                setSize(size + 1).then(c => {
-                    setShowLoading(false)
-                }).catch(c => {
-                    setShowLoading(false)
-                })
-            }
-        };
-    })
-    if (!data) return <Spinner showLoading='true'></Spinner>
-    return (
-
-        <Layout goBack='true'>
-            <div className="container-fluid px-1">
-                <div className="d-flex mb-3 p-2 bd-highlight flex-wrap justify-content-md-start justify-content-lg-start justify-content-sm-center justify-content-xs-center">
-                    {
-
-                        data.map((lists, index) => {
-                            return lists.map(d => <div className='p-2 animate__animated  animate__zoomIn' key={d.name}>
-                                <div className="card mb-3 p-2" style={{ width: '300px' }}>
-                                    <img src={d.image} className="card-img-top" style={{ maxWidth: '100%', height: 'auto' }} />
-                                    <hr></hr>
-                                    <div className="card-body">
-                                        <h5 className="card-title">{d.title}</h5>
-                                        {
-                                            d.count > 0 && <div className="skillbar clearfix ">
-                                                <div className="skillbar-title">
-                                                    <span>資料數量</span>
-                                                </div>
-                                                <div className="skillbar-bar"></div>
-                                                <div className="skill-bar-percent">{d.count}</div>
-                                            </div>
-                                        }
-                                        <button className="btn btn-warning"
-                                            onClick={() => {
-                                                router.push(
-                                                    {
-                                                        pathname: `/${serviceName}/dataset`,
-                                                        query: { queryUrl: d.url },
-                                                    }
-                                                )
-                                            }}
-                                        >
-                                            看更多<span className="material-icons" style={{ fontSize: '18px' }}>open_in_new</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>)
-                        })
-                    }
+      if (
+        window.innerHeight + window.scrollY - document.body.offsetHeight >
+        0
+      ) {
+        setShowLoading(true);
+        setSize(size + 1)
+          .then((c) => {
+            setShowLoading(false);
+          })
+          .catch((c) => {
+            setShowLoading(false);
+          });
+      }
+    };
+  });
+  if (!data) return <Spinner showLoading="true"></Spinner>;
+  return (
+    <Layout goBack="true">
+      <div className="container-fluid px-1">
+        <div className="d-flex mb-3 p-2 bd-highlight flex-wrap justify-content-md-start justify-content-lg-start justify-content-sm-center justify-content-xs-center">
+          {data.map((lists, index) => {
+            return lists.map((d) => (
+              <div
+                className="p-2 animate__animated  animate__zoomIn"
+                key={d.name}
+              >
+                <div className="card mb-3 p-2" style={{ width: "300px" }}>
+                  <img
+                    src={d.image}
+                    className="card-img-top"
+                    style={{ maxWidth: "100%", height: "auto" }}
+                  />
+                  <hr></hr>
+                  <div className="card-body">
+                    <h5 className="card-title">{d.title}</h5>
+                    {d.count > 0 && (
+                      <div className="skillbar clearfix ">
+                        <div className="skillbar-title">
+                          <span>資料數量</span>
+                        </div>
+                        <div className="skillbar-bar"></div>
+                        <div className="skill-bar-percent">{d.count}</div>
+                      </div>
+                    )}
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => {
+                        router.push({
+                          pathname: `/${serviceName}/dataset`,
+                          query: { queryUrl: d.url },
+                        });
+                      }}
+                    >
+                      看更多
+                      <span
+                        className="material-icons"
+                        style={{ fontSize: "18px" }}
+                      >
+                        open_in_new
+                      </span>
+                    </button>
+                  </div>
                 </div>
-            </div>
-            <Spinner showLoading={showLoading}></Spinner>
-        </Layout>
-    )
+              </div>
+            ));
+          })}
+        </div>
+      </div>
+      <Spinner showLoading={showLoading}></Spinner>
+    </Layout>
+  );
 }
--- website/pages/[serviceName]/dataset/detail/index.tsx
+++ website/pages/[serviceName]/dataset/detail/index.tsx
@@ -1,98 +1,171 @@
-import { useRouter } from "next/router"
-import React from "react"
-import useSWR from "swr"
-import CustomHeader from "../../../components/custom-header"
-import Layout from "../../../components/layout"
-import Spinner from "../../../components/spinner"
-const detailData = (serviceName: string | string[], pageUrl: string | string[]) => {
-    const { data, error, isValidating, mutate } = useSWR(
-        `/api/dataset/detail?serviceName=${serviceName}&pageUrl=${pageUrl}`,
-        fetcher,
-        {
-            refreshInterval: 60000
-        })
-    return { data, error, isValidating, mutate }
-}
-const fetcher = url => fetch(url).then(r => r.json())
+import { useRouter } from "next/router";
+import React from "react";
+import useSWR from "swr";
+import CustomHeader from "../../../components/custom-header";
+import Layout from "../../../components/layout";
+import Spinner from "../../../components/spinner";
+const detailData = (
+  serviceName: string | string[],
+  pageUrl: string | string[]
+) => {
+  const { data, error, isValidating, mutate } = useSWR(
+    `/api/dataset/detail?serviceName=${serviceName}&pageUrl=${pageUrl}`,
+    fetcher,
+    {
+      refreshInterval: 60000,
+    }
+  );
+  return { data, error, isValidating, mutate };
+};
+const fetcher = (url) => fetch(url).then((r) => r.json());
 export default function Index() {
-    const router = useRouter()
-    const { serviceName, queryUrl } = router.query
-    const fetchDetailData = detailData(serviceName, queryUrl)
-    console.log(fetchDetailData.data)
-    if (!fetchDetailData.data) return <Spinner showLoading='true'></Spinner>
-    return <Layout goBack='true'>
-
-        <div className="container-fluid px-1">
-
-            <div className="d-flex flex-column  p-3 mb-3 bd-highlight flex-wrap justify-content-center">
-                <div className="card  border-success bg-light">
-                    <div className="card-body">
-                        <h1>                {fetchDetailData.data.title}
-                        </h1>
-                    </div>
-                </div>
-                <hr></hr>
-
-                <div className="d-flex  flex-lg-row  flex-xl-row flex-xxl-row flex-sm-column  flex-column flex-xs-column p-3 mb-3 bd-highlight flex-wrap justify-content-center">
-                    {
-                        fetchDetailData.data.statics.map(s => <div className="card  border-info bg-light p-3 m-3">
-                            <div className="card-body">
-                                <h3 className="quote-text">                {s.name}
-                                </h3>
-                                <hr></hr>
-                                <h2 className="text-center">                {s.value}
-                                </h2>
-                            </div>
-                        </div>)
-                    }
-                </div>
-                <hr></hr>
-
-                <div className="list-group">
-                    {
-                        fetchDetailData.data.resources.map(r => <div className="list-group-item" style={{ borderBottom: '5px solid red' }}>
-                            <div className="d-flex justify-content-between">
-
-                                <div>{r.name}</div>
-                                <span className='label' data-format={r.type.toLowerCase()}>{r.type}</span>
-                            </div>
-                            <hr></hr>
-                            <blockquote>
-                                {r.description}
-                            </blockquote>
-                            <div className='p-3'></div>
-                            <div className="d-grid gap-2 d-md-block d-md-flex  justify-content-md-center">
-                                <button className='btn btn-info m-lg-2' onClick={
-                                    () => {
-                                        // Construct the 'a' element
-                                        const link = document.createElement("a");
-                                        link.download = 'download';
-                                        link.target = "_blank";
-
-                                        // Construct the URI
-                                        link.href = r.downloadLink;
-                                        document.body.appendChild(link);
-                                        link.click();
-
-                                        // Cleanup the DOM
-                                        document.body.removeChild(link);
-                                    }
-                                }>下載</button>
-                                <button className="btn btn-primary m-lg-2" type="button" onClick={() => {
-                                    window.open(r.detail);
-
-                                }}>明細</button>
-                            </div>
-                        </div>
-
-                        )
-                    }
-
-
-                </div>
-
-
+  const router = useRouter();
+  const { serviceName, queryUrl } = router.query;
+  const fetchDetailData = detailData(serviceName, queryUrl);
+  console.log(fetchDetailData.data);
+  if (!fetchDetailData.data) return <Spinner showLoading="true"></Spinner>;
+  return (
+    <Layout goBack="true">
+      <div className="container-fluid px-1">
+        <div className="d-flex flex-column  p-3 mb-3 bd-highlight flex-wrap justify-content-center">
+          <div className="card  border-success bg-light">
+            <div className="card-body">
+              <h1> {fetchDetailData.data.title}</h1>
             </div>
+          </div>
+          <hr></hr>
+
+          <div className="d-flex  flex-lg-row  flex-xl-row flex-xxl-row flex-sm-column  flex-column flex-xs-column p-3 mb-3 bd-highlight flex-wrap justify-content-center">
+            {fetchDetailData.data.statics.map((s) => (
+              <div className="card  border-info bg-light p-3 m-3">
+                <div className="card-body">
+                  <h3 className="quote-text"> {s.name}</h3>
+                  <hr></hr>
+                  <h2 className="text-center"> {s.value}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+          <hr></hr>
+          <div className="accordion" id="infomation">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseInfomation"
+                  aria-expanded="true"
+                  aria-controls="collapseInfomation"
+                >
+                  欄位資訊
+                </button>
+              </h2>
+              <div
+                id="collapseInfomation"
+                className="accordion-collapse collapse show"
+                aria-labelledby="headingOne"
+                data-bs-parent="#infomation"
+              >
+                <div className="accordion-body">
+                  <div className='p-3'>
+                    <div className="table-responsive">
+                      <table className="table table-bordered border-primary table-hover">
+                        <thead className="table-warning">
+                          <tr>
+                            <th scope="col">名稱</th>
+                            <th scope="col">值</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {fetchDetailData.data.infomation.map((s) => (
+                            <tr>
+                              <th scope="row">{s.name}</th>
+                              <td>{s.value}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr></hr>
+          <div className="accordion" id="fileList">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseFileList"
+                  aria-expanded="true"
+                  aria-controls="collapseFileList"
+                >
+                  檔案下載
+                </button>
+              </h2>
+              <div
+                id="collapseFileList"
+                className="accordion-collapse collapse show"
+                aria-labelledby="headingOne"
+                data-bs-parent="#fileList"
+              >
+                <div className="accordion-body">
+                  <div className="list-group">
+                    {fetchDetailData.data.resources.map((r) => (
+                      <div
+                        className="list-group-item"
+                        style={{ borderBottom: "5px solid red" }}
+                      >
+                        <div className="d-flex justify-content-between">
+                          <div>{r.name}</div>
+                          <span
+                            className="label"
+                            data-format={r.type.toLowerCase()}
+                          >
+                            {r.type}
+                          </span>
+                        </div>
+                        <hr></hr>
+                        <blockquote>{r.description}</blockquote>
+                        <div className="p-3"></div>
+                        <div className="d-grid gap-2 d-md-block d-md-flex  justify-content-md-center">
+                          <button
+                            className="btn btn-info m-lg-2"
+                            onClick={() => {
+                              const link = document.createElement("a");
+                              link.download = "download";
+                              link.target = "_blank";
+                              link.href = r.downloadLink;
+                              document.body.appendChild(link);
+                              link.click();
+                              document.body.removeChild(link);
+                            }}
+                          >
+                            下載
+                          </button>
+                          <button
+                            className="btn btn-primary m-lg-2"
+                            type="button"
+                            onClick={() => {
+                              window.open(r.detail);
+                            }}
+                          >
+                            明細
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </Layout>
+  );
 }
