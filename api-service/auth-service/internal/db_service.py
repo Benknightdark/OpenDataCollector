d29940ba5a42c4a8fcf38aa3e74285971f084687
BenKung <moon791017@gmail.com>
2021-04-05 12:59:59 +0800

    OK

---
+++ api-service/auth-service/internal/db_service.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import os
+from bson import json_util
+import json
+def convert_collection(data):
+    '''
+    轉換Collection資料形態
+    '''
+    return json.loads(json_util.dumps(data))
+def db(db_name):
+    '''
+    取得MongoDB 資料庫
+    '''
+    if os.getenv('ENVIRONMENT') == 'production':
+        db_uri =('mongodb://root:example@mongo/')
+    else:
+        db_uri =('mongodb://root:example@localhost:1769/')    
+    db_client = MongoClient(db_uri)
+    return db_client[db_name]
+def user_create(data):
+    '''
+    新增使用者資料
+    '''
+    create_data=db('auth')['user'].insert_one(data)
+    return create_data    
--- api-service/auth-service/main.py
+++ api-service/auth-service/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 import os 
+from internal import db_service
+
 class Login(BaseModel):
     userName: str
     password: str
@@ -14,7 +16,8 @@ else:
 
 @app.post("/api/login")
 def read_root(login_model: Login):
-    if login_model.userName=='fuck':      
+    if login_model.userName=='fuck': 
+        db_service.user_create({"userName":login_model.userName,"displayName":'wtf'})     
         return {"userName":login_model.userName,"displayName":'wtf'}
     else:
         raise HTTPException(status_code=404, detail="不存在此使用者")  
