776fec7d78fb8b4cb13aee00ba85b832291313c4
BenKung <moon791017@gmail.com>
2021-04-07 16:55:33 +0800

    aa

--- api-service/auth-service/install.sh
+++ api-service/auth-service/install.sh
@@ -1,3 +1,3 @@
-python -m pip install -r requirements.txt 
+# python -m pip install -r requirements.txt 
 
 python3.9 -m pip install -r requirements.txt
--- api-service/auth-service/main.py
+++ api-service/auth-service/main.py
@@ -1,24 +1,30 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import os 
+import os
 from internal import db_service
+import python_jwt as jwt
+import jwcrypto.jwk as jwk
+import datetime
+
 
 class Login(BaseModel):
     userName: str
     password: str
 
-if os.getenv("ENVIRONMENT")=='Production':
+
+if os.getenv("ENVIRONMENT") == 'Production':
 
     app = FastAPI(docs_url=None, redoc_url=None)
 else:
     app = FastAPI()
-  
+
 
 @app.post("/api/login")
 def read_root(login_model: Login):
-    if login_model.userName=='fuck': 
-        db_service.user_create({"userName":login_model.userName,"displayName":'wtf'})     
-        return {"userName":login_model.userName,"displayName":'wtf'}
+    
+    if login_model.userName == 'fuck':
+        db_service.user_create(
+            {"userName": login_model.userName, "displayName": 'wtf'})
+        return {"userName": login_model.userName, "displayName": 'wtf'}
     else:
-        raise HTTPException(status_code=404, detail="不存在此使用者")  
-
+        raise HTTPException(status_code=404, detail="不存在此使用者")
