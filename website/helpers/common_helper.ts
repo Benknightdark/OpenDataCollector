e1ee698acc0e70fca3a506a148887f999641acd4
BenKung <moon791017@gmail.com>
2021-04-07 14:03:24 +0800

    update envoy config file

--- website/helpers/common_helper.ts
+++ website/helpers/common_helper.ts
@@ -1,5 +1,6 @@
 
 export const getApiUrl=( apiName:string='')=>{
-   const url=process.env.API_URL!==undefined?`${process.env.API_URL}/invoke/${apiName}/method`:`http://localhost:${process.env[apiName.toUpperCase()]}`
+   const url=process.env.API_URL!==undefined?`${process.env.API_URL}/invoke/${apiName}/method`:`http://localhost:5202/${apiName.toLowerCase()}`
+   //`http://localhost:${process.env[apiName.toUpperCase()]}`
    return  url
 }
