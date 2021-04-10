913c0d6a947aa6eecdc92006b25f96c62b686991
BenKung <moon791017@gmail.com>
2021-04-10 14:32:55 +0800

    OK

--- website/helpers/common_helper.ts
+++ website/helpers/common_helper.ts
@@ -1,6 +1,7 @@
 
 export const getApiUrl=( apiName:string='')=>{
-   const url=process.env.API_URL!==undefined?`${process.env.API_URL}/invoke/${apiName}/method`:`http://localhost:5202/${apiName.toLowerCase()}`
-   //`http://localhost:${process.env[apiName.toUpperCase()]}`
+   const url=process.env.API_URL!==undefined?`http://localhost:3500/v1.0/invoke/api-gateway-service/method/${apiName}`:`http://localhost:5202/${apiName.toLowerCase()}`
+
+   // ${process.env.API_URL}/invoke/${apiName}/method`
    return  url
 }
