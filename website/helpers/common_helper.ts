50c53dde86caa1fe9811baac746a2496641f6ea6
BenKung <moon791017@gmail.com>
2021-04-10 14:34:05 +0800

    update getapiurl function

--- website/helpers/common_helper.ts
+++ website/helpers/common_helper.ts
@@ -1,6 +1,6 @@
 
 export const getApiUrl=( apiName:string='')=>{
-   const url=process.env.API_URL!==undefined?`http://localhost:3500/v1.0/invoke/api-gateway-service/method/${apiName}`:`http://localhost:5202/${apiName.toLowerCase()}`
+   const url=process.env.API_URL!==undefined?`${process.env.API_URL}/${apiName}`:`http://localhost:5202/${apiName.toLowerCase()}`
 
    // ${process.env.API_URL}/invoke/${apiName}/method`
    return  url
