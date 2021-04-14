4b2bd22ae1e3c34b38575740fee5a81cffb785a7
ben <moon791017@gmail.com>
2021-04-14 11:09:16 +0800

    update ocean wave css

--- website/pages/[serviceName]/dataset/detail/index.tsx
+++ website/pages/[serviceName]/dataset/detail/index.tsx
@@ -55,7 +55,7 @@ export default function Index() {
               <hr></hr>
             </div>
           )}
-          <div className="accordion border border-danger" id="infomation">
+          <div className="accordion border border-danger" id="infomation" style={{zIndex:100000}}>
             <div className="accordion-item">
               <h2 className="accordion-header" id="headingInfomation">
                 <button
