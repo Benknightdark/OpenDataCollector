261ec7f46246121d59580342f12c4dd87c848609
ben <moon791017@gmail.com>
2021-04-21 08:51:43 +0800

    update httpx to kao-service

--- api-service/kao-service/main.py
+++ api-service/kao-service/main.py
@@ -22,7 +22,6 @@ if os.getenv("ENVIRONMENT")=='Production':
 else:
     app = FastAPI()
   
-# app = FastAPI(docs_url=None, redoc_url=None)
 root_url = "https://data.kcg.gov.tw"
 
 
@@ -35,9 +34,10 @@ async def read_root():
     return {"Hello": "Kao Service"}
 
 @app.get("/api/dashboard", response_model=Dashboard, summary='取得高雄OpenData Dashboard資料')
-def dashboard():
+async def dashboard():
     dashboard_res_data = {}
-    r = requests.get(root_url)
+    client=httpx.AsyncClient(http2=True)
+    r = await client.get(root_url)
     soup = BeautifulSoup(r.text, 'html.parser')
     dashboard_root = soup.find_all(attrs={"class", "box stats"})[1]
     dashboard_title = dashboard_root.div.h3.text
@@ -57,18 +57,19 @@ def dashboard():
                 'count': item_count,
                 'name': item_name,
             })
+    await client.aclose()            
     return dashboard_res_data
 
 @app.get("/api/org", summary="組織列表")
-def org( page: Optional[int] = None):
+async def org( page: Optional[int] = None):
+    client=httpx.AsyncClient(http2=True)
     res_data = []
     page_str=''
     if(page!=None):
         page_str=f'?page={page}'
-    r = requests.get(f'{root_url}/organization{page_str}')
+    r = await client.get(f'{root_url}/organization{page_str}')
     soup = BeautifulSoup(r.text, 'html.parser')
     list_data = soup.find_all('li', attrs={'class': 'media-item'})
-    print(list_data)
     for l in list_data:
         zero_count = l.find('span', attrs={'class': "count"})
         data_count=0
@@ -79,35 +80,38 @@ def org( page: Optional[int] = None):
             'title': l.h3.text,
             'count':int(data_count) ,
             'url':f"{root_url}/{l.a['href']}"})
+    await client.aclose()            
     return res_data
 
 @app.get("/api/group", summary="群組列表")
-def group(page: Optional[int] = None):
+async def group(page: Optional[int] = None):
+    client=httpx.AsyncClient(http2=True)
     page_str=''
     if(page!=None):
         page_str=f'?page={page}'    
     res_data = []
-    r = requests.get(f'{root_url}/group{page_str}')
+    r = await client.get(f'{root_url}/group{page_str}')
     soup = BeautifulSoup(r.text, 'html.parser')
     list_data = soup.find_all('li', attrs={'class': 'media-item'})
-    print(list_data)
     for l in list_data:
         res_data.append({
             'image': l.img['src'],
             'title': l.h3.text,
             'url':f"{root_url}/{l.a['href']}"})
+    await client.aclose()            
     return res_data
 
 @app.get("/api/dataset", summary="資料集列表")
-def data_set(q: str):
+async def data_set(q: str):
     ''' 
     參數q的範例: 
     - https://data.kcg.gov.tw/dataset 
     - https://data.kcg.gov.tw/dataset?page=2 
     - https://data.kcg.gov.tw/dataset?q=%E9%87%91%E9%A1%8D 
     '''
+    client=httpx.AsyncClient(http2=True)
     res_data = {}
-    r = requests.get(q)
+    r = await client.get(q)
     soup = BeautifulSoup(r.text, 'html.parser')
     search_form = soup.find('form',attrs={'class':'search-form'})
     search_result = re.sub("\n|\r|\s+|-", '', search_form.h2.text)
@@ -122,16 +126,20 @@ def data_set(q: str):
         for dt in data_type:
             sl_data['data_type'].append(dt.a.text)
         res_data['data'].append(sl_data)
+    await client.aclose()            
+
     return res_data
 
 @app.get("/api/dataset/detail", summary="資料集明細")
-def data_set_detail(q: str):
+async def data_set_detail(q: str):
     ''' 
     參數q的範例: 
     - https://data.kcg.gov.tw/dataset/estimate-committee-propose 
     '''
+    client=httpx.AsyncClient(http2=True)
+
     res_data = {}
-    r = requests.get(q)
+    r = await client.get(q)
     soup = BeautifulSoup(r.text, 'html.parser')
     context_module=soup.find('div','context-info')
     res_data['title']=context_module.div.h1.text
@@ -161,4 +169,5 @@ def data_set_detail(q: str):
             'downloadLink':rl.div.ul.find_all('li')[1].a['href']
 
         })
+    await client.aclose()            
     return res_data
