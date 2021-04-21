4dd06a98c62f40485ed21f4847aad54f6f2c745a
ben <moon791017@gmail.com>
2021-04-21 09:24:21 +0800

    upate httpx package

--- api-service/account-service/main.py
+++ api-service/account-service/main.py
@@ -5,7 +5,6 @@ from internal import db_service,identity_service
 import python_jwt as jwt
 import jwcrypto.jwk as jwk
 import datetime
-import requests
 
 
 class Login(BaseModel):
@@ -26,13 +25,13 @@ else:
 
 
 @app.post("/api/login")
-def read_root(data: Login):
+async def read_root(data: Login):
     data_dict=data.dict()
     print(data)
     query_data=db_service.user_query(data_dict)
     print(query_data)
     if query_data !=None:
-        token=identity_service.token_endpoint(query_data['userName'],str(query_data['_id']['$oid']))  
+        token=await identity_service.token_endpoint(query_data['userName'],str(query_data['_id']['$oid']))  
         print(token)    
         return {"displayName": query_data['displayName'], "token": token['access_token']}
     else:
@@ -42,5 +41,5 @@ def read_root(data: Login):
 async def register(data:Register,summary="註冊"):
     data_dict=data.dict()
     create_data=db_service.user_create(data_dict)
-    token=identity_service.token_endpoint(data_dict['userName'],str(create_data.inserted_id))      
+    token=await identity_service.token_endpoint(data_dict['userName'],str(create_data.inserted_id))      
     return {"displayName": data_dict['displayName'], "token": token['access_token']}
--- api-service/personal-service/internal/secret_service.py
+++ api-service/personal-service/internal/secret_service.py
@@ -1,11 +1,16 @@
 import os
 import requests
-def get_jwt_secret():
+import httpx
+async def get_jwt_secret():
     if os.getenv('ENVIRONMENT') == 'production':
         new_key = f'jwtConfig:secret'
         url = (
             f'http://localhost:3500/v1.0/secrets/my-secret-store/{new_key}')
-        res = requests.get(url).json()[new_key]
-        return res
+        client = httpx.AsyncClient(http2=True)
+
+        res = (await client.get(url))
+        res_data=res.json()[new_key]
+        await res.aclose()
+        return res_data
     else:
         return "9e564bd21af4a8ba8fd0cde4c38cec3de51ae169b13339777f5fdbd4a044f22c"
