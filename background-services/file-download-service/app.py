786cf917ed45683f131bc2f717b2aa029edc3f0e
ben <moon791017@gmail.com>
2021-05-04 17:39:30 +0800

    find some way to download excel

--- background-services/file-download-service/app.py
+++ background-services/file-download-service/app.py
@@ -4,7 +4,7 @@ import httpx
 import csv
 import re
 import xmltodict, json
-
+import pyexcel as pe 
 def xml_to_json(file):
     obj = xmltodict.parse(file)
     return json.dumps(obj)
@@ -26,7 +26,8 @@ def csv_to_json(file):
 
 
 def xsl_to_json(file):
-    return file
+    sheet = pe.get_sheet(file_content=file)
+    return sheet.get_book_dict()
 
 
 def download(url, data_type, file_name):
@@ -35,22 +36,22 @@ def download(url, data_type, file_name):
     xml => json
     xsl => json
     '''
-    origin_data=httpx.get(url)
-
+    origin_data=None
     if data_type == 'csv':
-        print(data_type)
-        origin_data=csv_to_json(origin_data.text.strip().replace('\xa0', '').split(('\n')))
+        data=httpx.get(url)
+        origin_data=csv_to_json(data.text.strip().replace('\xa0', '').split(('\n')))
     if data_type == 'xml':
-        origin_data=xml_to_json(origin_data.text)
-        print(data_type)
-    if data_type == 'xsl':
-        print(data_type)
+        data=httpx.get(url)
+        origin_data=xml_to_json(data.text)
+    if data_type == 'xlsx':
+        data=httpx.get(url)
+        origin_data=xsl_to_json(data.text)
     print(origin_data)
 # download('https://quality.data.gov.tw/dq_download_csv.php?nid=138059&md5_url=0fa70d3715abf50fba7c12c6507fe7b4',
 #  'csv', '宜蘭縣勞工一般、特殊、巡迴體格及健康檢查指定醫院')   
 # download('https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=ead9c6e1-ab53-47b4-b539-5f05412d0ad3&rid=b999335c-827e-44a1-b554-29d1be3e59d1 ',
 #  'xml', '旅行時間偵測站設備資訊V2.0')  
-download('https://www.guanshan-land.gov.tw/uploads/110年關山鎮公告現值.xlsx','xlsx','關山鎮公告現值')
+download('https://www-ws.pthg.gov.tw/Upload/2015pthg/0/relfile/0/0/1240bd54-8fbd-4715-ab2b-9742a907b443.xls','xlsx','關山鎮公告現值')
 # def job():
 #     print("I'm working...")
 #     print('-----------------------------')
