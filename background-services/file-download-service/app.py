f2bbbb10097550de6df4d612e934217665a29dd2
ben <moon791017@gmail.com>
2021-05-05 09:10:49 +0800

    aa

--- background-services/file-download-service/app.py
+++ background-services/file-download-service/app.py
@@ -3,31 +3,45 @@ import time
 import httpx
 import csv
 import re
-import xmltodict, json
-import pyexcel as pe 
+import xmltodict
+import json
+import pyexcel as pe
+
+
 def xml_to_json(file):
     obj = xmltodict.parse(file)
     return json.dumps(obj)
 
 
 def csv_to_json(file):
-    new_data=[]
-    csv_data =list(csv.reader(file)) 
-    col_name=csv_data[0]
-    i=0
+    new_data = []
+    csv_data = list(csv.reader(file))
+    col_name = csv_data[0]
+    i = 0
     for c in csv_data:
-        if i!=0:
-            data={}
+        if i != 0:
+            data = {}
             for i in range(len(col_name)):
-                data[col_name[i]]=c[i]
+                data[col_name[i]] = c[i]
             new_data.append(data)
-        i=i+1
+        i = i+1
     return new_data
 
 
 def xsl_to_json(url):
     sheet = pe.get_sheet(url=url)
-    return sheet.array
+    sheet_array = sheet.array
+    new_data = []
+    col_name = sheet_array[0]
+    i = 0
+    for c in sheet_array:
+        if i != 0:
+            data = {}
+            for i in range(len(col_name)):
+                data[col_name[i]] = c[i]
+            new_data.append(data)
+        i = i+1
+    return new_data
 
 
 def download(url, data_type, file_name):
@@ -36,21 +50,24 @@ def download(url, data_type, file_name):
     xml => json
     xsl => json
     '''
-    origin_data=None
+    origin_data = None
     if data_type == 'csv':
-        data=httpx.get(url)
-        origin_data=csv_to_json(data.text.strip().replace('\xa0', '').split(('\n')))
+        data = httpx.get(url)
+        origin_data = csv_to_json(
+            data.text.strip().replace('\xa0', '').split(('\n')))
     if data_type == 'xml':
-        data=httpx.get(url)
-        origin_data=xml_to_json(data.text)
+        data = httpx.get(url)
+        origin_data = xml_to_json(data.text)
     if data_type == 'xlsx':
-        origin_data=xsl_to_json(url)
+        origin_data = xsl_to_json(url)
     print(origin_data)
+
+
 # download('https://quality.data.gov.tw/dq_download_csv.php?nid=138059&md5_url=0fa70d3715abf50fba7c12c6507fe7b4',
-#  'csv', '宜蘭縣勞工一般、特殊、巡迴體格及健康檢查指定醫院')   
+#  'csv', '宜蘭縣勞工一般、特殊、巡迴體格及健康檢查指定醫院')
 # download('https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=ead9c6e1-ab53-47b4-b539-5f05412d0ad3&rid=b999335c-827e-44a1-b554-29d1be3e59d1 ',
-#  'xml', '旅行時間偵測站設備資訊V2.0')  
-download('https://www-ws.pthg.gov.tw/Upload/2015pthg/0/relfile/0/0/1240bd54-8fbd-4715-ab2b-9742a907b443.xls','xlsx','關山鎮公告現值')
+#  'xml', '旅行時間偵測站設備資訊V2.0')
+download('https://www-ws.pthg.gov.tw/Upload/2015pthg/0/relfile/0/0/1240bd54-8fbd-4715-ab2b-9742a907b443.xls', 'xlsx', '關山鎮公告現值')
 # def job():
 #     print("I'm working...")
 #     print('-----------------------------')
