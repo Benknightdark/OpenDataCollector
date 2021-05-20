40f33ae16b2d8c3edd79b44af7a0efff11435852
ben <moon791017@gmail.com>
2021-05-20 10:06:01 +0800

    update secret service

--- api-service/account-service/internal/secret_service.py
+++ api-service/account-service/internal/secret_service.py
@@ -9,22 +9,13 @@ async def get_jwt_config():
                 f'http://localhost:3500/v1.0/secrets/my-secret-store/bulk')
         client = httpx.AsyncClient(http2=True)    
         res = await client.get(url)
-        res_data=res.text
+        res_data=res.json()
         await client.aclose()   
-        print(str(res_data))
         jwt_config_key = ["client", "scope", "secret"]
-
         jwt_config_data = {}
         for j in jwt_config_key:
-            new_key = f'jwtConfig:{j}'
-            url = (
-                f'http://localhost:3500/v1.0/secrets/my-secret-store/{new_key}')
-            client = httpx.AsyncClient(http2=True)    
-            res = await client.get(url)
-            res_data=res.json()[new_key]
-            jwt_config_data[j] = res_data
+            jwt_config_data[j] = res_data[f'jwtConfig:{j}'][f'jwtConfig:{j}']
             await client.aclose()
-            
         return jwt_config_data
     else:
         return {
