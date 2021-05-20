780341d683a9afa1aad61faddd888134f7dbd6e5
ben <moon791017@gmail.com>
2021-05-20 17:59:44 +0800

    aaa

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,26 +1,8 @@
 
 import '../styles/globals.css'
-function MyApp({ Component, pageProps }) {
-  // const router = useRouter()
-  // const protectedRouteArray = ['task']
 
-  // useEffect(() => {
-  //   const handleRouteChange = async (url) => {
-  //     if (url.toUpperCase().includes('LOGIN'.toUpperCase())) {
-  //       await checkIsLogin();
-  //     }else if (url.toUpperCase().includes('REGISTER'.toUpperCase())) {
-  //       await checkIsLogin();
-  //     } else {
-  //       protectedRouteArray.map(async p => {
-  //         if (url.toUpperCase().includes(p.toUpperCase())) {
-  //           await checkIsNotLogin();
-  //           console.log('OK')
-  //         }
-  //       })
-  //     }
-  //   }
-  //   handleRouteChange(router.pathname)
-  // })
+function MyApp({ Component, pageProps }) {
+  
 
   return <Component {...pageProps} />
 }
--- website/pages/components/layout.tsx
+++ website/pages/components/layout.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import CustomHeader from "./custom-header";
 import 'react-toastify/dist/ReactToastify.css';
 
 
 
+
 export default function Layout({ goBack = "false", children }) {
+
+
+
+
   return (
     <div>
       <CustomHeader goBack={goBack} />
       <main>{children}</main>
       <ToastContainer />
+      <div id='root'></div>
     </div>
   );
 }
