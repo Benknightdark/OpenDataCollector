c70fd48be89a89b1f68b84fcdd8d8cde419bc64c
ben <moon791017@gmail.com>
2021-05-24 08:54:16 +0800

    aa

--- api-service/identity-service/Startup.cs
+++ api-service/identity-service/Startup.cs
@@ -1,25 +1,14 @@
 
-using System;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
 using identity_service.Extensions;
 using identity_service.Middlewares;
 using identity_service.Services;
-using IdentityServer4.Configuration;
-using IdentityServer4.EntityFramework.DbContexts;
-using IdentityServer4.EntityFramework.Mappers;
-using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
-
 namespace identity_service
 {
     public class Startup
@@ -35,9 +24,6 @@ namespace identity_service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-
-
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddIdentityServer()
                 .AddConfigurationStore(options =>
@@ -53,10 +39,6 @@ namespace identity_service
             //.AddDeveloperSigningCredential()
             .AddCustomTokenRequestValidator<CustomTokenRequestValidator>();
             services.AddHttpClient<SecretService>();
-
-
-
-
         }
         private async Task InitializeDatabase(IApplicationBuilder app)
         {
@@ -70,16 +52,12 @@ namespace identity_service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             Task.Run(async () =>
           {
               await InitializeDatabase(app);
           });
-
-            app.UseDeveloperExceptionPage();
+            // app.UseDeveloperExceptionPage();
             app.UseIdentityServer();
-
-
         }
     }
 }
