771631eb40fb955b78ed51efc9dc977f3edb22c3
ben <moon791017@gmail.com>
2021-05-28 15:33:48 +0800

    UPDATE

--- api-service/task-service/internal/db_service.py
+++ api-service/task-service/internal/db_service.py
@@ -3,6 +3,7 @@ import os
 from bson import json_util
 import json
 from bson.objectid import ObjectId
+import datetime
 
 
 def convert_collection(data):
@@ -18,9 +19,9 @@ def db(db_name):
     '''
     if os.getenv('ENVIRONMENT') == 'production':
         if os.getenv('MONGODB'):
-            db_uri =os.getenv('MONGODB')
+            db_uri = os.getenv('MONGODB')
         else:
-            db_uri =('mongodb://root:example@mongo/')
+            db_uri = ('mongodb://root:example@mongo/')
     else:
         db_uri = ('mongodb://root:example@localhost:1769/')
     db_client = MongoClient(db_uri)
@@ -78,5 +79,38 @@ def delete_schedule(data_id):
     '''
     delete_data = db('task')['schedule'].update_one(
         {'data': {'$elemMatch': {'_id': ObjectId(data_id)}}},
-         {'$pull': {'data': {"_id": ObjectId(data_id)}}}, True)
+        {'$pull': {'data': {"_id": ObjectId(data_id)}}}, True)
     return delete_data.upserted_id
+
+
+def history_query(user_id, schedule_id):
+    history_data = db('task')['history'].find_one(
+        {"userId": user_id, "scheduleId": schedule_id})
+    return convert_collection(history_data)
+
+
+def add_history(user_id, schedule_id, origin_data):
+    inesert_data = db('task')['history'].insert_one({
+        "userId": user_id,
+        "scheduleId": schedule_id,
+        "data": [
+            {
+                "createdTime": datetime.datetime.now(),
+                "record": origin_data
+            }
+        ]
+    })
+    return inesert_data.inserted_id
+
+
+def update_history(user_id, schedule_id, origin_data):
+    update_data = db('task')['history'].update_one({"userId": user_id, "scheduleId": schedule_id}, {
+        '$push': {
+            'data': {
+                "createdTime": datetime.datetime.now(),
+                "record": origin_data
+            }
+        }
+    },
+        True)
+    return update_data.raw_result
--- website/pages/api/task/execute.ts
+++ website/pages/api/task/execute.ts
@@ -6,9 +6,9 @@ import { getApiUrl } from "../../../helpers/common_helper";
 export default async (req, res) => {
     try {
         const session = await getSession({ req })
-        const url=`${getApiUrl("file-download-service")}/scheduler/jobs/${req.query['id']}/run`;
+        const url = `${getApiUrl("file-download-service")}/scheduler/jobs/${req.query['id']}/run`;
         const reqData = await fetch(url, {
-            method:'POST',
+            method: 'POST',
             headers: new Headers({
                 'Authorization': 'Bearer ' + session.user.token,
             }),
@@ -16,10 +16,10 @@ export default async (req, res) => {
         const resData = await reqData.json()
         res.status(200).json(resData)
     } catch (error) {
-        res.status(500).json({error:error})
+        res.status(500).json({ error: error })
 
     }
-   
+
 }
 
 
