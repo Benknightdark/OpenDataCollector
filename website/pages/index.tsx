cf5d08e36d07aa69d9657cc1062fc0a52bf192a9
ben <moon791017@gmail.com>
2021-05-31 17:25:43 +0800

    add grid test

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,17 +1,60 @@
 
 import React from 'react'
 import Dashboard from './components/dahsboard'
-
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
 export default function Home() {
+  const classes = useStyles();
+
   return (
-
-
-    <div className='row  bd-highlight'>
-      <Dashboard serviceName='kao-service' />
-      <Dashboard serviceName='tainan-service' />
-      <Dashboard serviceName='taichung-service' />
-      <Dashboard serviceName='pthg-service' />
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
     </div>
+  );
+  // return (
 
-  )
+
+  //   <div className='row  bd-highlight'>
+  //     <Dashboard serviceName='kao-service' />
+  //     <Dashboard serviceName='tainan-service' />
+  //     <Dashboard serviceName='taichung-service' />
+  //     <Dashboard serviceName='pthg-service' />
+  //   </div>
+
+  // )
 }
