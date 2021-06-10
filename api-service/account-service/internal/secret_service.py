70cb753c20056246a643da042feb91ed754eba57
ben <moon791017@gmail.com>
2021-06-10 15:37:05 +0800

    OK

--- api-service/account-service/internal/secret_service.py
+++ api-service/account-service/internal/secret_service.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import httpx
 async def get_jwt_config():
@@ -10,20 +9,12 @@ async def get_jwt_config():
                 "secret":os.getenv("SECRET")
             }            
         else:
-
-            url = (
-                    f'http://localhost:3500/v1.0/secrets/my-secret-store/bulk')
+            url = f'http://localhost:3500/v1.0/secrets/kubernetes/opendatasecrets'
             client = httpx.AsyncClient(http2=True)    
             res = await client.get(url)
             res_data=res.json()
-            print(res_data)
             await client.aclose()   
-            jwt_config_key = ["client", "scope", "secret"]
-            jwt_config_data = {}
-            for j in jwt_config_key:
-                jwt_config_data[j] = res_data[f'jwtConfig:{j}'][f'jwtConfig:{j}']
-                await client.aclose()
-            return jwt_config_data
+            return res_data
     else:
         return {
             "client": "client",
