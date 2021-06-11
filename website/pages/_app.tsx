d34dc819cd437ce3d422f1d56ee2786c96a976bf
ben <moon791017@gmail.com>
2021-06-11 16:28:57 +0800

    OK

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -4,30 +4,28 @@ import '../styles/globals.css'
 import Layout from './components/layout'
 import { SnackbarProvider } from 'notistack';
 import { Button } from '@material-ui/core';
-const notistackRef = React.createRef<SnackbarProvider>();
-const onClickDismiss = key => () => { 
-  notistackRef.current.closeSnackbar(key);
-}
-function MyApp({ Component, pageProps }) {
 
-  
-  return  <SnackbarProvider maxSnack={3} 
-  ref={notistackRef}
-  
-  action={(key) => (
+function MyApp({ Component, pageProps }) {
+  const notistackRef = React.createRef<SnackbarProvider>();
+  const onClickDismiss = key => () => {
+    notistackRef.current.closeSnackbar(key);
+  }
+  return <SnackbarProvider maxSnack={3}
+    ref={notistackRef}
+    action={(key) => (
       <Button variant="contained" color="primary" component="span" onClick={onClickDismiss(key)}>
-          關閉
+        關閉
       </Button>
-  )}
-  anchorOrigin={{
-    vertical: 'top',
-    horizontal: 'right',
-}}>
-  <Layout>
-    <div className="container-fluid px-1">
-      <Component {...pageProps} />
-    </div>
-  </Layout>
+    )}
+    anchorOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}>
+    <Layout>
+      <div className="container-fluid px-1">
+        <Component {...pageProps} />
+      </div>
+    </Layout>
   </SnackbarProvider>
 }
 
